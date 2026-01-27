7cc0ec3aa253e9d0d8a98682c2766150d55e4706
MD Emon Sarkar <emonsarkar.emo683@gmail.com>
2026-01-27 19:00:32 +0600

    new

--- Exam/src/exam/Exam.java
+++ Exam/src/exam/Exam.java
@@ -1,29 +1,45 @@
-
 package exam;
 
 import java.util.Scanner;
 
-
 public class Exam {
 
-   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-//        System.out.println("Enter first Value");
-//        int a = sc.nextInt();
-//        
-//        System.out.println("Enter Second Value");
-//        int b = sc.nextInt();
-//        
-//       
-//        System.out.println("Sum of the numbers: " + (a + b));
 
+        // Take two integers and sum of them. 1st Question
+        /*
+        System.out.println("Enter first Value");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Value");
+        int b = sc.nextInt();
+        System.out.println("Sum of the numbers: " + (a + b));
+        */
+        
+        // inputed name printed with welcome text. 2nd Question
+        /*
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
-        
+
         System.out.println("Welcome " + name);
+        */
         
+        
+        
+        
+        System.out.println("Enter a number");
+        float radius = sc.nextFloat();
+        float areaOfCircle;
+        
+        if(radius <= 0){
+            System.out.println("Enter a valid number");
+        } else {
+            areaOfCircle = (float)(Math.PI * Math.pow(radius, 2));
+            System.out.println("Area of a Circle is " + areaOfCircle);
+        }
+        
+        
+
     }
-    
+
 }
---
+++ LeetCodeProblem/src/leetcodeproblem/LeetCodeProblem.java
@@ -0,0 +1,86 @@
+package leetcodeproblem;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map;
+    
+
+public class LeetCodeProblem {
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        converter(scanner.nextLine());
+        String[] strs = {"ab", "a"};
+        longestCommonPrefix(strs);
+    }
+
+    // convert roman number to number
+    static void converter(String s) {
+    String[] arrays = {"I", "V", "X", "L", "C", "D", "M"};
+    int[] intArray = {1, 5, 10, 50, 100, 500, 1000};
+
+    
+    Map<Character, Integer> map = new HashMap<>();
+    map.put('I', 1);
+    map.put('V', 5);
+    map.put('X', 10);
+    map.put('L', 50);
+    map.put('C', 100);
+    map.put('D', 500);
+    map.put('M', 1000);
+
+    int sum = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int value = map.get(s.charAt(i));
+        
+        if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
+            sum -= value;
+        } else {
+            sum += value;
+        }
+        
+    }
+    
+ 
+
+    
+}
+
+    // isPalindrome
+    public static boolean isPalindrome(int x) {
+        int rem;
+        int a = x;
+        int rev = 0;
+        while (x > 0) {
+            rem = x % 10;
+            rev = (rev * 10) + rem;
+            x /= 10;
+        }
+
+        boolean e;
+        e = rev == a;
+        return e;
+    }
+    
+    public static void longestCommonPrefix(String[] strs) {
+        
+        String st = "";
+       
+       
+        
+        for(int i = 1; i < strs.length; i++){
+            if(strs[i].startsWith(st)){
+                        
+            } else {
+                st = "";
+                break;
+            }
+            
+        }
+        System.out.println(st);
+        
+    }
+    
+    
+
+}
